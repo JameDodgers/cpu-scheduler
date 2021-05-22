10014c04d32f088572443155eb37a29f5909f6dd
Pablo Henrique <pablohenrique.r.c@gmail.com>
2021-05-22 20:50:23 -0300

    Executa o algoritmo de escalonamento escolhido
    Se não há mais processos em execução e nenhum outro processo está programado para chegar, então o contador de tempo é interrompido.

--- src/functions/algorithms.js
+++ src/functions/algorithms.js
@@ -1,55 +1,54 @@
-export const fifo = (tasks) => {
-    if (tasks.length > 0) {
-        const task = tasks.shift();
-        console.log(task);
-        return task;
+export const fifo = (queue) => {
+  if (queue.length > 0) {
+    const task = queue[0];
+
+    --task.executionTime;
+
+    if (task.executionTime === 0) {
+      queue.splice(0, 1);
     }
-}
+    return task;
+  }
 
-
+  return undefined
+};
 
 // SJF (Shortest Job First) - retorna para o processador a tarefa mais curta na fila de tarefas prontas
 export const sjf = (tasks) => {
-    var shortestTask = tasks[0];
-    for(let i = 1; i < tasks.length; i++) {
-        if (tasks[i].executionTime < shortestTask.executionTime) {
-            shortestTask = tasks[i];
-        }
+  var shortestTask = tasks[0];
+  for (let i = 1; i < tasks.length; i++) {
+    if (tasks[i].executionTime < shortestTask.executionTime) {
+      shortestTask = tasks[i];
     }
-    console.log(shortestTask);
-    return shortestTask;
-}
-
+  }
+  console.log(shortestTask);
+  return shortestTask;
+};
 
 // Round Robin - fifo com tempo de quantum
 export const roundRobin = (tasks, quantum) => {
-    var task;
-    var time;
-    if (tasks.length > 0) {
-        task = tasks.shift();
+  var task;
+  var time;
+  if (tasks.length > 0) {
+    task = tasks.shift();
 
-        if(task.executionTime >= quantum) {
-            time = quantum;
-            task.executionTime -= quantum;
-        }
-        else {
-            time = task.executionTime;
-            task.executionTime -= quantum;
-        }
-
-
-        if( task.executionTime > 0)
-            tasks.push(task);
+    if (task.executionTime >= quantum) {
+      time = quantum;
+      task.executionTime -= quantum;
+    } else {
+      time = task.executionTime;
+      task.executionTime -= quantum;
     }
 
-    
-    console.log(task);
-    console.log(tasks);
-    return {task, time};
-}
+    if (task.executionTime > 0) tasks.push(task);
+  }
+
+  console.log(task);
+  console.log(tasks);
+  return { task, time };
+};
 
 // EDF (Earliest Deadline First) - retorna para o processador a tarefa com menor dealine na fila de prontos
 export const edf = (tasks) => {
-    
-    return
-}
+  return;
+};
--- src/screens/PlayGround.jsx
+++ src/screens/PlayGround.jsx
@@ -1,151 +1,167 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import * as ScreenOrientation from 'expo-screen-orientation';
+import * as ScreenOrientation from "expo-screen-orientation";
 
-import { useDimensions } from '@react-native-community/hooks'
+import { useDimensions } from "@react-native-community/hooks";
 
 import { 
-  StyleSheet,
-  View,
-  ScrollView
-} from 'react-native'
+  StyleSheet, 
+  View, 
+  ScrollView 
+} from "react-native";
 
 // import RAM from '../components/RAM'
-import Gantt from '../components/Gantt'
-import { fifo, roundRobin, sjf } from '../functions/algorithms';
+import Gantt from "../components/Gantt";
+import { fifo, roundRobin, sjf } from "../functions/algorithms";
 
-import dimensions from '../util/dimensions'
+import dimensions from "../util/dimensions";
 
-const tasks = [
-  {
+// const tasks = [
+//   {
+//     id: 1,
+//     arrivalTime: 0,
+//     executionTime: 3,
+//     deadline: 3,
+//     priority: 1,
+//   },
+//   {
+//     id: 2,
+//     arrivalTime: 0,
+//     executionTime: 3,
+//     deadline: 6,
+//     priority: 1,
+//   },
+//   {
+//     id: 3,
+//     arrivalTime: 0,
+//     executionTime: 3,
+//     deadline: 9,
+//     priority: 1,
+//   },
+//   {
+//     id: 4,
+//     arrivalTime: 0,
+//     executionTime: 2,
+//     deadline: 11,
+//     priority: 1,
+//   },
+// ];
+
+const index = ({ route }) => {
+  const {
+    quantum,
+    overload,
+    tasks,
+    selectedPagingAlgorithm,
+    selectedSchedulingAlgorithm,
+  } = route.params;
+
+  const { height: width } = useDimensions().window;
+
+  const [time, setTime] = useState(0);
+  const [executedTask, setExecutedTask] = useState({
     id: 1,
-    arrivalTime: 0,
-    executionTime: 3,
-    deadline: 0,
-    priority: 1,
-  },
-  {
-    id: 2,
-    arrivalTime: 0,
-    executionTime: 3,
-    deadline: 1,
-    priority: 1,
-  },
-  {
-    id: 3,
     arrivalTime: 0,
     executionTime: 3,
     deadline: 2,
     priority: 1,
-  },
-  {
-    id: 4,
-    arrivalTime: 0,
-    executionTime: 2,
-    deadline: 3,
-    priority: 1,
-  }
-]
+  });
+  const [columnsNumber, setColumnsNumber] = useState();
+  const [executionInterval, setExecutionInterval] = useState(null);
+  const [queue, _] = useState(Array());
 
-const index = ({route}) => {
-  const {
-    quantum,
-    overload,
-    // tasks,
-    selectedPagingAlgorithm,
-    selectedSchedulingAlgorithm,
-  } = route.params
-  
-  const { 
-    height: width
-  } = useDimensions().window
+  const schedulingAlgorithms = [fifo, sjf, roundRobin];
 
-  const [time, setTime] = useState(0)
-  const [executedTask, setExecutedTask] = useState({
-      id: 1,
-      arrivalTime: 0,
-      executionTime: 3,
-      deadline: 2,
-      priority: 1,
-    })
-  const [columnsNumber, setColumnsNumber] = useState()
-  const [executionInterval, setExecutionInterval] = useState(null)
+  const schedulingAlgorithm =
+    schedulingAlgorithms[selectedSchedulingAlgorithm - 1];
 
-  const scheduling = () => {
-    const activeTasks = tasks.filter(((task) => task.arrivalTime >= time))
+  let longestArrivalTime = 0
 
-    // console.log(`${time}`)
-
-    // Para não fazer o gráfico crescer indefinidamente
-    if(time === 40){
-      setExecutedTask(undefined)
-      clearInterval(executionInterval)
-      console.log('stop')
-    }
-
-    // setExecutedTask(roundRobin(tasks, 2));
+  for(let i = 0; i < tasks.length; i++){
+    if(tasks[i].arrivalTime > longestArrivalTime)
+    longestArrivalTime = tasks[i].arrivalTime
   }
 
   useEffect(() => {
-    // console.log(time)
-    scheduling()
-  }, [time])
-
-  const execute = () => {
-    setTime(time => time + 1)
-  }
-
-  useEffect(() => {   
-    setExecutionInterval(setInterval(execute, 500))
+    const scheduling = () => {
+      if(executedTask === undefined && time > longestArrivalTime) {
+        clearInterval(executionInterval)
+        return;
+      }
+  
+      tasks.forEach((task, _) => {
+        if (
+          !(queue.some((item) => item.id === task.id)) &&
+          task.arrivalTime === time
+        ) {
+          queue.push(Object.assign({}, task))
+        }
+      });
+  
+      setExecutedTask(schedulingAlgorithm(queue));
+    };
     
+    scheduling();
+  }, [time]);
+
+  useEffect(() => {
+    const execute = () => {
+      setTime((time) => time + 1);
+    };
+
+    setExecutionInterval(setInterval(execute, 500));
+
     return () => {
-      clearInterval(executionInterval)
-    }
-  }, [])
+      clearInterval(executionInterval);
+    };
+  }, []);
 
   const changeScreenOrientation = async () => {
-    try{
-      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
+    try {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.LANDSCAPE_LEFT
+      );
 
-      const columnsNumber = Math.floor(width / (dimensions.cellSize + dimensions.cellBorderSize)) - 5
-      setColumnsNumber(columnsNumber)
-    }catch(error){
-      console.log(error)
+      const columnsNumber =
+        Math.floor(width / (dimensions.cellSize + dimensions.cellBorderSize)) - 5;
+
+      setColumnsNumber(columnsNumber);
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    changeScreenOrientation()
+    changeScreenOrientation();
 
     return () => {
-      ScreenOrientation.unlockAsync()
-    }
-  }, [])
+      ScreenOrientation.unlockAsync();
+    };
+  }, []);
 
-  return(
+  return (
     <View style={styles.container}>
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}>
-        {
-          (columnsNumber !== undefined) && (
-            <Gantt
-              tasks={tasks}
-              time={time}
-              executedTask={executedTask}
-              columnsNumber={columnsNumber}
-            />
-          )
-        }
+      <ScrollView 
+        horizontal 
+        showsHorizontalScrollIndicator={false}
+      >
+        {columnsNumber !== undefined && (
+          <Gantt
+            tasks={tasks}
+            time={time}
+            executedTask={executedTask}
+            columnsNumber={columnsNumber}
+          />
+        )}
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
-})
+  },
+});
 
-export default index
+export default index;
