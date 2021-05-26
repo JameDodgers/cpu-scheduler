d1ce454d327386644b715b88a99fa7d36122c4a5
Pablo Henrique <pablohenrique.r.c@gmail.com>
2021-05-25 22:36:00 -0300

    Exibe estouro de deadline no gráfico

--- src/components/Gantt.jsx
+++ src/components/Gantt.jsx
@@ -1,117 +1,101 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import {
-  StyleSheet,
-  View
-} from 'react-native'
+import { StyleSheet, View } from "react-native";
 
-import Cell from './Cell'
+import Cell from "./Cell";
 
-export default ({tasks, executedTask, time, columnsNumber}) => {
-  const [maxTime, setMaxTime] = useState(columnsNumber)
+export default ({ tasks, executedTask, time, columnsNumber }) => {
+  const [maxTime, setMaxTime] = useState(columnsNumber);
   const [matrix, setMatrix] = useState(
-    Array.from({length: tasks.length}, () => 
-      Array.from({length: columnsNumber}, () => 0)
+    Array.from({ length: tasks.length }, () =>
+      Array.from({ length: columnsNumber }, () => 0)
     )
-  )
+  );
 
   useEffect(() => {
-    const draw = () => {  
-      if(time > columnsNumber) {
+    const draw = () => {
+      if (time > columnsNumber) {
         const updatedMatrix = matrix.map((task, _) => {
-          task.push(0)
-          return task
-        })
-        
-        setMatrix(updatedMatrix)
-        setMaxTime(time)
+          task.push(0);
+          return task;
+        });
+
+        setMatrix(updatedMatrix);
+        setMaxTime(time);
       }
-      
-      if(executedTask !== undefined) {
 
-        const updatedMatrix = matrix
-        updatedMatrix[executedTask.id - 1][time - 1] = 
-          executedTask.overload ? 2 : 1 
+      if (executedTask !== undefined) {
+        const updatedMatrix = matrix;
+        updatedMatrix[executedTask.id - 1][time - 1] = executedTask.overload
+          ? 2
+          : (executedTask.deadline > time)
+          ? 3
+          : 1;
 
-        setMatrix(updatedMatrix)
+        setMatrix(updatedMatrix);
       }
-    }
+    };
 
-    draw()
-  }, [time])
+    draw();
+  }, [time]);
 
-  return(
+  return (
     <View style={styles.container}>
       <View>
-        {
-          tasks.map((task, row) => (
-            <View 
-              key={`infoS${row}`}
-              style={styles.row}>
-              <View style={styles.row}>
-                <Cell text={task.arrivalTime} />
-                <Cell text={task.executionTime} />
-                <Cell text={task.deadline} />
-                <Cell text={task.priority} />
-                <Cell text={task.id} />
-              </View>
+        {tasks.map((task, row) => (
+          <View key={`infoS${row}`} style={styles.row}>
+            <View style={styles.row}>
+              <Cell text={task.arrivalTime} />
+              <Cell text={task.executionTime} />
+              <Cell text={task.deadline} />
+              <Cell text={task.priority} />
+              <Cell text={task.id} />
             </View>
-          ))
-        }
+          </View>
+        ))}
         <View style={styles.row}>
-          <Cell text='TC' />
-          <Cell text='TE' />
-          <Cell text='D' />
-          <Cell text='P' />
-          <Cell text='ID' />
+          <Cell text="TC" />
+          <Cell text="TE" />
+          <Cell text="D" />
+          <Cell text="P" />
+          <Cell text="ID" />
         </View>
-      </View> 
+      </View>
       <View>
-        {
-          matrix.map((task, row) => {
-            const { deadline } = tasks[row]
-            
-            return (
-              <View 
-                key={`${row}`}
-                style={styles.row}>
-                  {
-                    task.map((cell, column) => (
-                      <Cell
-                        style={[
-                          (column === deadline - 1) && { borderEndColor: 'red'},
-                          (cell === 1) && { backgroundColor: 'green'},
-                          (cell === 2) && { backgroundColor: 'red'}
-                        ]}
-                        key={`${row}${column}`} 
-                      />
-                    ))
-                  }                
-              </View>            
-            )
-          })
-        }
+        {matrix.map((task, row) => {
+          const { deadline } = tasks[row];
+
+          return (
+            <View key={`${row}`} style={styles.row}>
+              {task.map((cell, column) => (
+                <Cell
+                  style={[
+                    column === deadline - 1 && { borderEndColor: "red" },
+                    cell === 1 && { backgroundColor: "green" },
+                    cell === 2 && { backgroundColor: "red" },
+                    cell === 3 && { backgroundColor: "orange" },
+                  ]}
+                  key={`${row}${column}`}
+                />
+              ))}
+            </View>
+          );
+        })}
         <View style={styles.row}>
-          {
-            [...Array(Number(maxTime))]
-            .map((_, index) => (
-              <Cell 
-                key={`time${index + 1}`}  
-                text={`${index + 1}`} 
-              />
-            ))
-          }
+          {[...Array(Number(maxTime))].map((_, index) => (
+            <Cell key={`time${index + 1}`} text={`${index + 1}`} />
+          ))}
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
-})
+});
