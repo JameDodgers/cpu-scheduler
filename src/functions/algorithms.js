164edbbe4759d2d71c469fd3031cd3fea735cc33
gisellyreis <gisellyareis@gmail.com>
2021-05-25 23:39:30 -0300

    resolve algoritmo roundRobin

--- src/functions/algorithms.js
+++ src/functions/algorithms.js
@@ -55,6 +55,10 @@ export const sjf = (tasks, queue, time) => {
   return undefined;
 };
 
+function multiplo(n, mult) {
+  if((mult % n) === 0) return true;
+  else return false;
+}
 
 // Round Robin - fifo com tempo de quantum
 export const roundRobin = (tasks, queue, time, quantum, quantumCount, setQuantumCount) => {
@@ -92,36 +96,25 @@ export const roundRobin = (tasks, queue, time, quantum, quantumCount, setQuantum
 // EDF (Earliest Deadline First) - retorna para o processador a tarefa com menor dealine na fila de prontos
 export const edf = (tasks, queue, time, quantum, quantumCount, setQuantumCount) => {
   if (queue.length > 0) {
-    var task = queue[0];
+    var shortestTask = queue[0];
+    console.log(queue);
 
-    for(let i=0; i < queue.length; i++) {
-      if(queue[i].deadline < task.deadline) {
+    // Seleciona o processo com menor dealine
+    for (let i = 0; i < queue.length; i++) {
+      if (queue[i].deadline < shortestTask.deadline) {
+        shortestTask = queue[i];
       }
     }
 
-    if(!tasks[task.id - 1].startExecutionTime) {
-      tasks[task.id - 1].startExecutionTime = time + 1
-    }
+    --shortestTask.executionTime;
 
-    //console.log(quantumCount);
-    setQuantumCount(quantumCount => quantumCount -1);
-    --task.executionTime;
-
-    if(quantumCount === 1  && task.executionTime > 0) {
-      //console.log('entrou');
-      queue.splice(0, 1);
-      queue.push(task);
-      setQuantumCount(quantum);
+    // Tira da fila se acabou o tempo de excução
+    if (shortestTask.executionTime === 0) {
+      queue.splice(queue.indexOf(shortestTask), 1);
     }
-
-    if (task.executionTime === 0) {
-      queue.splice(0, 1);
-      tasks[task.id - 1].endExecutionTime = time + 1;
-      setQuantumCount(quantum);
-    }
-    
+    //console.log(shortestTask);
     return {
-      ...task,
+      ...shortestTask,
       overload: false
     };;
   }
