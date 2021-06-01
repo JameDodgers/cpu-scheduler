2da30831abf25099e255ef093ad3d7fca68f6ae6
Pablo Henrique <pablohenrique.r.c@gmail.com>
2021-06-01 12:14:21 -0300

    Conserta algoritmos EDF e Priority

--- src/functions/algorithms.js
+++ src/functions/algorithms.js
@@ -2,32 +2,31 @@ export const fifo = (tasks, queue, time) => {
   if (queue.length > 0) {
     const task = queue[0];
 
-    if(!tasks[task.id - 1].startExecutionTime) {
-      tasks[task.id - 1].startExecutionTime = time + 1
+    if (!tasks[task.id - 1].startExecutionTime) {
+      tasks[task.id - 1].startExecutionTime = time + 1;
     }
 
     --task.executionTime;
 
     if (task.executionTime === 0) {
       queue.splice(0, 1);
-      tasks[task.id - 1].endExecutionTime = time + 1
+      tasks[task.id - 1].endExecutionTime = time + 1;
     }
     return {
       ...task,
-      overload: false
+      overload: false,
     };
   }
 
-  return undefined
+  return undefined;
 };
 
 // SJF (Shortest Job First) - retorna para o processador a tarefa mais curta na fila de tarefas prontas
 export const sjf = (tasks, queue, time) => {
-
   if (queue.length > 0) {
     var shortestTask = queue[0];
 
-    console.log(tasks[shortestTask.id - 1])
+    console.log(tasks[shortestTask.id - 1]);
 
     for (let i = 0; i < queue.length; i++) {
       if (queue[i].executionTime < shortestTask.executionTime) {
@@ -35,20 +34,20 @@ export const sjf = (tasks, queue, time) => {
       }
     }
 
-    if(!tasks[shortestTask.id - 1].startExecutionTime) {
-      tasks[shortestTask.id - 1].startExecutionTime = time + 1
+    if (!tasks[shortestTask.id - 1].startExecutionTime) {
+      tasks[shortestTask.id - 1].startExecutionTime = time + 1;
     }
 
     --shortestTask.executionTime;
 
     if (shortestTask.executionTime === 0) {
       queue.splice(queue.indexOf(shortestTask), 1);
-      tasks[shortestTask.id - 1].endExecutionTime = time + 1
+      tasks[shortestTask.id - 1].endExecutionTime = time + 1;
     }
     //console.log(shortestTask);
     return {
       ...shortestTask,
-      overload: false
+      overload: false,
     };
   }
 
@@ -56,127 +55,153 @@ export const sjf = (tasks, queue, time) => {
 };
 
 // Round Robin - fifo com tempo de quantum
-export const roundRobin = (tasks, queue, time, quantum, quantumCount, setQuantumCount) => {
+export const roundRobin = (
+  tasks,
+  queue,
+  time,
+  quantum,
+  quantumCount,
+  setQuantumCount
+) => {
   if (queue.length > 0) {
     let task = queue[0];
 
-    for(let i = 0; i < queue.length; i++) {
-      if((tasks[queue[i].id - 1].endExecutionTime < tasks[task.id - 1].endExecutionTime)) {
+    for (let i = 0; i < queue.length; i++) {
+      if (
+        tasks[queue[i].id - 1].endExecutionTime <
+        tasks[task.id - 1].endExecutionTime
+      ) {
         task = queue[i];
       }
     }
 
-    if(!tasks[task.id - 1].startExecutionTime) {
-      tasks[task.id - 1].startExecutionTime = time + 1
+    if (!tasks[task.id - 1].startExecutionTime) {
+      tasks[task.id - 1].startExecutionTime = time + 1;
     }
 
-    setQuantumCount(quantumCount => quantumCount - 1);
+    setQuantumCount((quantumCount) => quantumCount - 1);
 
     --task.executionTime;
 
-    if(quantumCount === 1  && task.executionTime > 0) {
+    if (quantumCount === 1 && task.executionTime > 0) {
       queue.splice(queue.indexOf(task), 1);
       queue.push(task);
       tasks[task.id - 1].endExecutionTime = time + 1;
       setQuantumCount(quantum);
     }
-    
+
     if (task.executionTime === 0) {
       queue.splice(queue.indexOf(task), 1);
       tasks[task.id - 1].endExecutionTime = time + 1;
       setQuantumCount(quantum);
     }
-    
+
     return {
       ...task,
-      overload: false
+      overload: false,
     };
   }
 
-  return undefined
+  return undefined;
 };
 
 // EDF (Earliest Deadline First) - retorna para o processador a tarefa com menor dealine na fila de prontos
-export const edf = (tasks, queue, time, quantum, quantumCount, setQuantumCount) => {
+export const edf = (
+  tasks,
+  queue,
+  time,
+  quantum,
+  quantumCount,
+  setQuantumCount
+) => {
   if (queue.length > 0) {
-    var task = queue[0];
+    let task = queue[0];
 
-    // Seleciona o processo com menor dealine
-    for(let i = 0; i < queue.length; i++) {
-      if (queue[i].deadline < task.deadline) {
-        task = queue[i];
+    if (quantumCount === quantum) {
+      // Seleciona o processo com menor dealine
+      for (let i = 0; i < queue.length; i++) {
+        if (queue[i].deadline < task.deadline) {
+          task = queue[i];
+        }
       }
-    }
-    
-    if(!tasks[task.id - 1].startExecutionTime) {
-      tasks[task.id - 1].startExecutionTime = time + 1
+
+      queue.splice(queue.indexOf(task), 1);
+      queue.splice(0, 0, task);
     }
 
-    //console.log(quantumCount);
-    setQuantumCount(quantumCount => quantumCount -1);
+    if (!tasks[task.id - 1].startExecutionTime) {
+      tasks[task.id - 1].startExecutionTime = time + 1;
+    }
+
+    setQuantumCount((quantumCount) => quantumCount - 1);
     --task.executionTime;
 
-    if(quantumCount === 1  && task.executionTime > 0) {
-      queue.splice(queue.indexOf(task), 1);
-      queue.push(task);
+    if (quantumCount === 1 && task.executionTime > 0) {
       setQuantumCount(quantum);
     }
 
     // Tira da fila se acabou o tempo de excução
     if (task.executionTime === 0) {
-      queue.splice(queue.indexOf(task), 1);
+      queue.splice(0, 1);
       tasks[task.id - 1].endExecutionTime = time + 1;
       setQuantumCount(quantum);
     }
 
-    //console.log(task);
     return {
       ...task,
-      overload: false
-    };;
+      overload: false,
+    };
   }
 
   return undefined;
 };
 
 // Escalonamento por Prioridade - retorna para o processador a tarefa com a maior prioridade na fila de prontos
-export const priority = (tasks, queue, time, quantum, quantumCount, setQuantumCount) => {
+export const priority = (
+  tasks,
+  queue,
+  time,
+  quantum,
+  quantumCount,
+  setQuantumCount
+) => {
   if (queue.length > 0) {
     var task = queue[0];
 
-    // Seleciona o processo com maior prioridade
-    for(let i = 0; i < queue.length; i++) {
-      if (queue[i].priority > task.priority) {
-        task = queue[i];
+    if (quantumCount === quantum) {
+      // Seleciona o processo com maior prioridade
+      for (let i = 0; i < queue.length; i++) {
+        if (queue[i].priority > task.priority) {
+          task = queue[i];
+        }
       }
-    }
-    
-    if(!tasks[task.id - 1].startExecutionTime) {
-      tasks[task.id - 1].startExecutionTime = time + 1
+
+      queue.splice(queue.indexOf(task), 1);
+      queue.splice(0, 0, task);
     }
 
-    //console.log(quantumCount);
-    setQuantumCount(quantumCount => quantumCount -1);
+    if (!tasks[task.id - 1].startExecutionTime) {
+      tasks[task.id - 1].startExecutionTime = time + 1;
+    }
+
+    setQuantumCount((quantumCount) => quantumCount - 1);
     --task.executionTime;
 
-    if(quantumCount === 1  && task.executionTime > 0) {
-      queue.splice(queue.indexOf(task), 1);
-      queue.push(task);
+    if (quantumCount === 1 && task.executionTime > 0) {
       setQuantumCount(quantum);
     }
 
     // Tira da fila se acabou o tempo de excução
     if (task.executionTime === 0) {
-      queue.splice(queue.indexOf(task), 1);
+      queue.splice(0, 1);
       tasks[task.id - 1].endExecutionTime = time + 1;
       setQuantumCount(quantum);
     }
 
-    //console.log(task);
     return {
       ...task,
-      overload: false
-    };;
+      overload: false,
+    };
   }
 
   return undefined;
